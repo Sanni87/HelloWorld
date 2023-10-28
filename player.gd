42b1c28c0c911129ed14bf50d0bb41015e95a99a
Sanni87 <asier.sannicolas@gmail.com>
2023-10-28 18:41:27 +0200

    creada escena de enemigo

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal hit
 
 @export var speed = 400 #How fast the player will move
 @export var green = true #Is player green?
@@ -8,6 +9,7 @@ var screen_size #Size of the game window
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	#pass # Replace with function body.
 
 
@@ -28,3 +30,26 @@ func _process(delta):
 		$AnimatedSprite2D.play() #Mirar en la docu que es esto. Pone que es un shorthand de get_node("AnimatedSprite2D").play()
 	else:
 		$AnimatedSprite2D.stop()
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred (at the end of the current frame) as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
